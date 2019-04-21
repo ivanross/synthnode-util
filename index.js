43121984728df7ad63ee9780f96697a9f295a1e6
Ivan <ivanross.me@gmail.com>
2019-04-21 19:16:00 +0200

    🐞 ♻️ fixed default options and  refactor test

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const Generator = require("audio-generator");
 const Speaker = require("audio-speaker");
 
-module.exports.mono = function mono(osc, { duration = Infinity }) {
+module.exports.mono = function mono(osc, options = { duration: Infinity }) {
   function aux(t) {
     return [osc.tf(t)];
   }
-  return new Generator(aux, { duration }).pipe(new Speaker());
+  return new Generator(aux, options).pipe(new Speaker());
 };
 
-module.exports.stereo = function stereo(left, right, { duration = Infinity }) {
+module.exports.stereo = function stereo(
+  left,
+  right,
+  options = { duration: Infinity }
+) {
   function aux(t) {
     return [left.tf(t), right.tf(t)];
   }
-  return new Generator(aux, { duration }).pipe(new Speaker());
+  return new Generator(aux, options).pipe(new Speaker());
 };
--- test.js
+++ test.js
@@ -3,13 +3,13 @@ const { expect } = require("chai");
 const { Oscillator } = require("synthnode");
 const { mono, stereo } = require("./index");
 
-const osc = new Oscillator({ frequency: 220, amplitude: 0.5 });
+const osc = new Oscillator({ frequency: 220, amplitude: 0.1 });
 
 describe("Tests", () => {
   it("mono", () => {
     expect(() => mono(osc, { duration: 1 })).to.not.throw();
   });
   it("stereo", () => {
-    expect(() => stereo(osc, osc, { duration: 11 })).to.not.throw();
+    expect(() => stereo(osc, osc, { duration: 1 })).to.not.throw();
   });
 });

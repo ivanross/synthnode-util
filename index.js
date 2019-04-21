e225a7b402574bf0e861a0bf42b7fffd8c983a69
Ivan <ivanross.me@gmail.com>
2019-04-21 11:36:20 +0200

    📈 add travis

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const Generator = require("audio-generator");
 const Speaker = require("audio-speaker");
 
-module.exports.mono = function(osc) {
+module.exports.mono = function mono(osc) {
   function aux(t) {
     return [osc.tf(t)];
   }
   return new Generator(aux, { duration: Infinity }).pipe(new Speaker());
 };
 
-module.exports.stereo = function(left, right) {
+module.exports.stereo = function stereo(left, right) {
   function aux(t) {
     return [left.tf(t), right.tf(t)];
   }
